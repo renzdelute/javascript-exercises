4cf651695900a298d25f31719d9a3257be8b2002
renzdelute <renzdelute18@gmail.com>
2026-07-01 23:36:04 +0800

    update

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,10 +1,12 @@
 const reverseString = function(str) {
     let reversedString = "";
+    
     for(let i = str.length - 1; i >= 0; i--){
         reversedString += str[i];
     }
+
     return reversedString;
 }; 
 
-// Do not edit below this linejiji
+// Do not edit below this line
 module.exports = reverseString;
--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
 const removeFromArray = function() {
+    const newArray = [1, 2, 3, 4];
+
+    
 };
 
 // Do not edit below this line
