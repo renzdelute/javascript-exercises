3669007eb790c97a90e15c97e380872c40572250
renzdelute <renzdelute18@gmail.com>
2026-07-02 18:09:19 +0800

    update

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,8 +1,8 @@
-const removeFromArray = function() {
-    const newArray = [];
-
-    i
+const removeFromArray = function(array, ...args) {
+    return array.filter(item => !args.includes(item));
 };
+
+console.log(removeFromArray([1,2,3,4,5,6], 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
